d77dceec261e82d29ad4e346b2310210ed728b76
samat_sultanov <samatsayak@gmail.com>
2023-02-07 13:08:13 +0600

    создал сериализатор для комментариев

--- source/api_v2/serializers.py
+++ source/api_v2/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from webapp.models import Article
+from webapp.models import Article, Comment
 
 
 class ArticleSerializer(serializers.ModelSerializer):
@@ -7,3 +7,10 @@ class ArticleSerializer(serializers.ModelSerializer):
         model = Article
         fields = ['id', 'title', 'content', 'author', 'tags', 'created_at', 'updated_at']
         read_only_fields = ['id', 'author', 'tags', 'created_at', 'updated_at']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'text', 'article', 'author', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'author', 'created_at', 'updated_at']
