4d6dede98827d4a08d74bbd3b6a0c613efa8abce
samat_sultanov <samatsayak@gmail.com>
2023-02-07 13:09:12 +0600

    добавил урлы для комментариев

--- source/api_v2/urls.py
+++ source/api_v2/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path, include
-from api_v2.views import ArticleView
+
+from api_v2.views import ArticleView, CommentView
 
 app_name = 'api_v2'
 
 article_url = [
     path('', ArticleView.as_view()),
-    path('<int:pk>/', ArticleView.as_view())
+    path('<int:pk>/', ArticleView.as_view()),
+]
+
+comment_url = [
+    path('', CommentView.as_view()),
+    path('<int:pk>/', CommentView.as_view()),
 ]
 
 urlpatterns = [
-    path('article/', include(article_url))
+    path('article/', include(article_url)),
+    path('comment/', include(comment_url)),
 ]
